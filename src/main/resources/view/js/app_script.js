dfbe0b3e9a713482b8a1e2401cd217355a45ec35
BloodBaron <barontm@gmail.com>
2019-09-13 16:41:09 +0200

    Changing to ChartJS

--- src/main/resources/view/js/app_script.js
+++ src/main/resources/view/js/app_script.js
@@ -16,14 +16,35 @@ function drawChart() {
         ['Task', 'Hours per Day'],
         ['Work',     11],
         ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
+        ['Eat',      2],
         ['Commute',  2],
         ['Watch TV', 2],
+        ['Cos', 1],
+        ['Cos innego', 1.5],
         ['Sleep',    7]
     ]);
 
     var options = {
         is3D: true,
-        backgroundColor: { fill:'transparent' }
+        backgroundColor: { fill:'transparent' },
+        legend: {
+            position: 'labeled',
+            textStyle: {
+                fontSize: 15,
+                bold: true
+            },
+        },
+        pieSliceText: 'percentage',
+        // sliceVisibilityThreshold: 0.05,
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
